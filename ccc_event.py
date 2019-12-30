eee439bc9b15eedc90813f2886f0d484257a4301
mxmo0rhuhn <code@schrimpf.ch>
2019-12-30 16:08:05 +0100

    reformat

--- ccc_event.py
+++ ccc_event.py
@@ -1,69 +1,72 @@
 #!/usr/bin/python3
 
-import os
-import sys
-import shutil
-import urllib3
-import base64
 import argparse
+import base64
+import os
+import shutil
+import sys
 from xml.dom.minidom import parse
+
+import urllib3
 
 urllib3.disable_warnings()
 
 DOWNLOADS_DIR = '/tmp'
 
 
-
 def get_file(url):
-#    name = url.rsplit('/', 1)[-1]
-#   Easy and works
-    name = str(base64.b64encode(bytes(url, "utf-8")),"utf-8")
+    # name = url.rsplit('/', 1)[-1]
+    # Easy and works
+    name = str(base64.b64encode(bytes(url, "utf-8")), "utf-8")
 
-# Combine the name and the downloads directory to get the local filename
+    # Combine the name and the downloads directory to get the local filename
     filename = os.path.join(DOWNLOADS_DIR, name)
 
-# Download the file if it does not exist
+    # Download the file if it does not exist
     if not os.path.isfile(filename):
-        if not quiet: 
+        if not quiet:
             print("Downloading %s " % (url))
-        with http.request('GET', url, preload_content=False) as r, open(filename, 'wb') as out_file:       
-           shutil.copyfileobj(r, out_file)
+        with http.request('GET', url, preload_content=False) as r, open(filename, 'wb') as out_file:
+            shutil.copyfileobj(r, out_file)
 
     return filename
 
-#def find_talk(fahrplan, talk_name, media_url, response): 
-def find_talk(fahrplan, media, query): 
+
+# def find_talk(fahrplan, talk_name, media_url, response):
+def find_talk(fahrplan, media, query):
     fahrplan_xml = parse(fahrplan)
     media_xml = parse(media)
 
     for fahrplan_node in fahrplan_xml.getElementsByTagName("title"):
         if str.upper(query) in str.upper(fahrplan_node.childNodes[0].data):
+            print("Found: " + str.upper(fahrplan_node.childNodes[0].data))
             title = fahrplan_node.childNodes[0].data
 
             #  Event
             par = fahrplan_node.parentNode
             event_id = par.getAttribute('id')
 
-            try: 
+            try:
                 sub = par.getElementsByTagName("subtitle")[0].childNodes[0].data
-            except: 
+            except:
                 sub = ""
-                if not quiet: 
+                if not quiet:
                     print("Element has no subtitle")
 
             try:
-                speakers = ", ".join([speaker.childNodes[0].data for speaker in par.getElementsByTagName("persons")[0].getElementsByTagName("person")])
+                speakers = ", ".join([speaker.childNodes[0].data for speaker in
+                                      par.getElementsByTagName("persons")[0].getElementsByTagName("person")])
 
             except:
                 speakers = ""
                 if not quiet:
                     print("Element has no speakers")
 
-            try: 
+            try:
                 desc = par.getElementsByTagName("description")[0].childNodes[0].data
-            except: 
+            except:
                 desc = None
-                if not quiet: 
+                if not quiet:
                     print("Element has no description")
 
             for media_node in media_xml.getElementsByTagName("title"):
@@ -91,7 +94,7 @@ def find_talk(fahrplan, media, query):
                         media_url = enclosure.getAttribute("url")
                         media_type = enclosure.getAttribute("length")
                         media_length = enclosure.getAttribute("type")
-                    except: 
+                    except:
                         print("Could not get media information")
                         exit()
 
@@ -129,9 +132,12 @@ parser = argparse.ArgumentParser(description='Search for talks in CCC events and
 
 parser.add_argument('query', metavar='<query string>', type=str, help='The search string')
 parser.add_argument('-q', '--quiet', dest='quiet', action='store_true', help='Disable optional output')
-parser.add_argument('-l', '--long', dest='long_desc', action='store_true', help='Print long description from Fahrplan instead of short descriptions from CCC media feed.')
-parser.add_argument('-y', '--year', metavar='year', dest='year', type=str, default="2016", help='Year to search (currently 2014, 2015 and 2016 are supported). Defaults to 2016.')
-parser.add_argument('-c', '--comment', metavar='text', dest='custom_comment', type=str, default="", help='Custom comment to add to the description.')
+parser.add_argument('-l', '--long', dest='long_desc', action='store_true',
+                    help='Print long description from Fahrplan instead of short descriptions from CCC media feed.')
+parser.add_argument('-y', '--year', metavar='year', dest='year', type=str, default="2016",
+                    help='Year to search (currently 2014, 2015 and 2016 are supported). Defaults to 2016.')
+parser.add_argument('-c', '--comment', metavar='text', dest='custom_comment', type=str, default="",
+                    help='Custom comment to add to the description.')
 
 args = parser.parse_args()
 
@@ -141,10 +147,10 @@ long_desc = args.long_desc
 year = args.year
 custom_comment = args.custom_comment
 
-# 33c3 specific:
-podcat_feed = "https://media.ccc.de/c/33c3/podcast/mp4.xml"
-fahrplan_location = "https://fahrplan.events.ccc.de/congress/2016/Fahrplan/schedule.xml"
-event_pattern_head = "https://fahrplan.events.ccc.de/congress/2016/Fahrplan/events/"
+# 36c3 specific:
+podcat_feed = "https://media.ccc.de/podcast-hq.xml"
+fahrplan_location = "https://fahrplan.events.ccc.de/congress/2019/Fahrplan/schedule.xml"
+event_pattern_head = "https://fahrplan.events.ccc.de/congress/2019/Fahrplan/events/"
 event_pattern_tail = ".html"
 
 if year == "2015":
